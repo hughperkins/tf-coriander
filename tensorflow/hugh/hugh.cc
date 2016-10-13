3332d501f05e3dd99f4089a96fff37d9fd0a2701
Hugh Perkins <hughperkins@gmail.com>
2016-10-13 09:18:14 +0000

    cocl in hugh.cc seems to work, somewhat unexpectedly

--- tensorflow/hugh/hugh.cc
+++ tensorflow/hugh/hugh.cc
@@ -2,12 +2,17 @@
 #include "tensorflow/core/graph/graph.h"
 #include <iostream>
 
+#include <cuda_runtime.h>
+
 using namespace std;
 using namespace tensorflow;
 
 int main(int argc, char *argv[]) {
     cout << "hugh" << endl;
     Graph graph(OpRegistry::Global());
+    float *gpuFloats;
+    const int N = 1024;
+    cudaMalloc((void **)(&gpuFloats), N * sizeof(float));
     cout << "all done" << endl;
     return 0;
 }
