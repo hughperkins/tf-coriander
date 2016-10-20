3b3f1786dfc9eba54f5f18f0197fdaace5898971
Hugh Perkins <hughperkins@gmail.com>
2016-10-20 13:01:44 +0100

    bunch of fixes in the updated cuda-on-cl commit

--- tensorflow/core/platform/default/stream_executor.h
+++ tensorflow/core/platform/default/stream_executor.h
@@ -22,7 +22,7 @@ limitations under the License.
 #include "tensorflow/core/platform/default/from_stream_executor_status.h"
 #include "tensorflow/stream_executor/device_memory.h"
 #include "tensorflow/stream_executor/dnn.h"
-#include "tensorflow/stream_executor/dso_loader.h"
+// #include "tensorflow/stream_executor/dso_loader.h"
 #include "tensorflow/stream_executor/event.h"
 #include "tensorflow/stream_executor/lib/status.h"
 #include "tensorflow/stream_executor/multi_platform_manager.h"
