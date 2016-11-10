26a6f8f47200674d912bdd57b0ed8e72cd50239b
Hugh Perkins <hughperkins@gmail.com>
2016-11-10 11:19:56 +0000

    add uint8, int32 to test_binary.py

--- tensorflow/stream_executor/cl/test/test_binary_ops.py
+++ tensorflow/stream_executor/cl/test/test_binary_ops.py
@@ -17,33 +17,56 @@ funcs = {
 }
 
 
+def nop(values):
+    return values
+
+
 def get_test_funcs():
     tests = []
-    for tf_func, py_func in funcs.items():
-        tests.append({'tf_func': tf_func, 'py_func': py_func})
+    for dtype in ['uint8', 'float32', 'int32']:
+        for tf_func, py_func in funcs.items():
+            if 'int' in dtype and tf_func in ['not_equal', 'pow']:
+                continue
+            if dtype == 'uint8' and tf_func in ['maximum', 'minimum', 'squared_difference', 'sub', 'add']:
+                continue
+            test = {'dtype': dtype, 'tf_func': tf_func, 'py_func': py_func}
+            test['mark'] = nop
+            if dtype == 'uint8' and tf_func in ['mul', 'div']:
+                test['mark'] = pytest.mark.xfail
+            tests.append(test)
     return tests
 
 
-@pytest.mark.parametrize('tf_func,py_func', [(d['tf_func'], d['py_func']) for d in get_test_funcs()])
-def test(tf_func, py_func):
-    print('func', tf_func)
+@pytest.mark.parametrize(
+    'dtype,tf_func,py_func',
+    [d['mark']((d['dtype'], d['tf_func'], d['py_func'])) for d in get_test_funcs()])
+def test(dtype, tf_func, py_func):
+    print('func', tf_func, dtype)
+    np_dtype = eval('np.%s' % dtype)
+    tf_dtype = eval('tf.%s' % dtype)
     with tf.Graph().as_default():
         with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
             with tf.device('/gpu:0'):
-                tf_a = tf.placeholder(tf.float32, [None, None], 'a')
-                tf_b = tf.placeholder(tf.float32, [None, None], 'b')
+                tf_a = tf.placeholder(tf_dtype, [None, None], 'a')
+                tf_b = tf.placeholder(tf_dtype, [None, None], 'b')
                 tf_c = tf.__dict__[tf_func](tf_a, tf_b, name="c")
 
             np.random.seed(123)
             shape = (1, 10)
             a = np.random.choice(50, shape) / 25
             b = np.random.choice(50, shape) / 25
+            if 'int' in dtype:
+                a *= 10
+                b *= 10
+            a = a.astype(np_dtype)
+            b = b.astype(np_dtype)
 
             ar, br, cr = sess.run((tf_a, tf_b, tf_c), {tf_a: a, tf_b: b})
-            print('ar', ar)
-            print('br', br)
-            print('cr', cr)
-            c_py = eval(py_func)
+            print('a', ar)
+            print('b', br)
+            c_py = eval(py_func).astype(np_dtype)
+            print('expected', c_py)
+            print('gpu', cr)
             diff = np.abs(c_py - cr).max()
             print('diff', diff)
             assert diff < 1e-4, 'failed for %s' % tf_func
