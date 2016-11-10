77dd8028df3fad13e71b6eb9ff77c3fd580f4cec
Hugh Perkins <hughperkins@gmail.com>
2016-11-10 11:56:16 +0000

    add test_softmax.py (xfails with batch_size > 1)

---
+++ tensorflow/stream_executor/cl/test/test_softmax.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import pytest
+
+
+def softmax(x):
+    batch_size = x.shape[0]
+    e_x = np.exp(x - np.max(x, 1).reshape(batch_size, -1))
+    return e_x / e_x.sum(1).reshape(batch_size, -1)
+
+
+@pytest.mark.parametrize('size', [
+        (1, 3),
+        pytest.mark.xfail((2, 3)),
+        pytest.mark.xfail((3, 10))
+])
+def test_softmax(size):
+    print('size', size)
+    with tf.Graph().as_default():
+        with tf.device('/gpu:0'):
+            tf_a = tf.placeholder(tf.float32, [None, None], 'a')
+            tf_out = tf.nn.softmax(tf_a, name="out")
+
+            np.random.seed(123)
+            # shape = (3, 10)
+            # shape = (3, 10)
+            a = np.random.uniform(10, size=size)
+            expected = softmax(a)
+            with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
+                gpu_out = sess.run(tf_out, {tf_a: a})
+            print('a', a)
+            print('expected', expected)
+            print('gpu', gpu_out)
+            diff = np.abs(gpu_out - expected).max()
+            print('diff', diff)
+            assert diff <= 1e-4
