078125c9111a806ef46797165ea2939442b40aee
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 14:05:42 +0000

    tweak install-python.sh

--- travis/install-python.sh
+++ travis/install-python.sh
@@ -8,14 +8,14 @@ PYTHON_DIR=/Library/Frameworks/Python.framework/Versions/3.5
 
 bash ${BASEDIR}/travis/download.sh pythoninstall ${PYTHON_DIR}
 
-if [[ ! -f ${PYTHON_DIR}/bin/python ]]; then {
+if [[ ! -f ${PYTHON_DIR}/bin/python3.5 ]]; then {
     cd ${BASEDIR}
-    get https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
+    wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
     sudo installer -pkg python-3.5.2-macosx10.6.pkg -target /
     python3 -V
     which python3
-    ls /usr/local/bin
-    ls /usr/local/lib
+    ls -lh /usr/local/bin/python*
+    ls -lhd /usr/local/lib/py*
     find /usr/local -name 'dist-packages'
     pip3 install --upgrade setuptools
     pip3 install --upgrade wheel
