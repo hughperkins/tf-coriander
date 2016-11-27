b1106215e34668f3880656ca6801559fc233a226
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 15:50:31 +0000

    update python install (in travis)

--- travis/install-python.sh
+++ travis/install-python.sh
@@ -6,7 +6,9 @@ cd ${BASEDIR}
 
 PYTHON_DIR=/Library/Frameworks/Python.framework/Versions/3.5
 
-bash ${BASEDIR}/travis/download.sh pythoninstall ${PYTHON_DIR}
+bash ${BASEDIR}/travis/download.sh pythoninstall /tmp/pycopy
+sudo mkdir -p ${PYTHON_DIR}
+sudo rsync -a /tmp/pycopy/ ${PYTHON_DIR}/
 
 wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
 sudo installer -pkg python-3.5.2-macosx10.6.pkg -target /
@@ -26,5 +28,6 @@ if [[ ${NUMPY_COUNT} == 0 ]]; then {
     pip3 install --upgrade wheel
     pip3 install --upgrade pip
     pip3 install numpy
-    bash ${BASEDIR}/travis/upload.sh pythoninstall ${PYTHON_DIR}
+    rsync -a ${PYTHON_DIR}/ /tmp/pycopy/
+    bash ${BASEDIR}/travis/upload.sh pythoninstall /tmp/pycopy
 } fi
