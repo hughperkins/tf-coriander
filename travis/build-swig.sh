1a964ced8d34c3c024406628d2e30d838c45d967
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 14:09:24 +0000

    fix swig path

--- travis/build-swig.sh
+++ travis/build-swig.sh
@@ -5,7 +5,7 @@ set -x
 cd ${BASEDIR}
 
 echo BASEDIR ${BASEDIR}
-bash ${BASEDIR}/travis/download.sh swig ${BASEDIR}/soft/swig
+bash ${BASEDIR}/travis/download.sh swig ${BASEDIR}/soft/swig-3.0.10
 if [[ ! -f ${BASEDIR}/soft/swig-3.0.10/swig ]]; then {
     cd ${BASEDIR}/soft
     wget http://kent.dl.sourceforge.net/project/swig/swig/swig-3.0.10/swig-3.0.10.tar.gz
@@ -14,7 +14,7 @@ if [[ ! -f ${BASEDIR}/soft/swig-3.0.10/swig ]]; then {
     cd swig-3.0.10
     ./configure
     make -j 8
-    bash ${BASEDIR}/travis/upload.sh swig ${BASEDIR}/soft/swig
+    bash ${BASEDIR}/travis/upload.sh swig ${BASEDIR}/soft/swig-3.0.10
 } fi
 cd ${BASEDIR}/soft/swig-3.0.10
 sudo make install
