b401e33246c1ff135476acede5f11f4d1f38a737
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 15:14:31 +0000

    tweak py install

--- travis/install-python.sh
+++ travis/install-python.sh
@@ -8,10 +8,14 @@ PYTHON_DIR=/Library/Frameworks/Python.framework/Versions/3.5
 
 bash ${BASEDIR}/travis/download.sh pythoninstall ${PYTHON_DIR}
 
-if [[ ! -f ${PYTHON_DIR}/bin/python3.5 ]]; then {
+wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
+sudo installer -pkg python-3.5.2-macosx10.6.pkg -target /
+
+NUMPY_COUNT=$(pip3 freeze | grep numpy | wc -l)
+echo NUMPY_COUNT ${NUMPY_COUNT}
+
+if [[ ${NUMPY_COUNT} == 0 ]]; then {
     cd ${BASEDIR}
-    wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
-    sudo installer -pkg python-3.5.2-macosx10.6.pkg -target /
     python3 -V
     which python3
     ls -lh /usr/local/bin/python*
