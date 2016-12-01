c520cc15628bc9a7e4d24e7ba267186f95992022
Hugh Perkins <hughperkins@gmail.com>
2016-12-01 14:26:51 +0000

    tweak python install

--- travis/install-python.sh
+++ travis/install-python.sh
@@ -7,16 +7,18 @@ cd ${BASEDIR}
 PYTHON_DIR=/Library/Frameworks/Python.framework/Versions/3.5
 
 bash ${BASEDIR}/travis/download.sh pythoninstall /tmp/pycopy
+ls /tmp/pycopy
 sudo mkdir -p ${PYTHON_DIR}
 sudo rsync -a /tmp/pycopy/ ${PYTHON_DIR}/
 
 wget https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg
 sudo installer -pkg python-3.5.2-macosx10.6.pkg -target /
 
-NUMPY_COUNT=$(pip3 freeze | grep numpy | wc -l)
-echo NUMPY_COUNT ${NUMPY_COUNT}
+NUMPY_COUNT=`pip3 freeze | grep numpy | wc -l`
+echo NUMPY_COUNT [${NUMPY_COUNT}]
 
-if [[ ${NUMPY_COUNT} == 0 ]]; then {
+if [ ${NUMPY_COUNT} -eq 0 ]; then {
+    echo installing numpy
     set -x
     cd ${BASEDIR}
     python3 -V
