a364749a0c7161eef700fdf579bc8431d09362a9
Hugh Perkins <hughperkins@gmail.com>
2017-06-02 01:10:47 +0100

    improve test_random.py somewaht

--- tensorflow/stream_executor/cl/test/test_random.py
+++ tensorflow/stream_executor/cl/test/test_random.py
@@ -11,44 +11,46 @@ shapes = [
     (50, 70, 12)
 ]
 
+seed = 123
 
-def test_random_func(func_name, shape):
+
+def _test_random_func(func_name, shape):
     print('func_name', func_name)
     func = eval(func_name)
     with tf.Graph().as_default():
         with tf.device('/cpu:0'):
-            W_t = tf.Variable(func(shape, seed=123))
+            W_t = tf.Variable(func(shape, seed=seed))
 
             with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
                 sess.run(tf.initialize_all_variables())
                 W_cpu = sess.run(W_t)
         with tf.device('/gpu:0'):
-            W_t = tf.Variable(func(shape, seed=123))
+            W_t = tf.Variable(func(shape, seed=seed))
 
             with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
                 sess.run(tf.initialize_all_variables())
                 W_gpu = sess.run(W_t)
-            if np.prod(W_gpu.shape) < 20:
+            if np.prod(np.array(shape)) < 20:
                 print('W_cpu', W_cpu)
                 print('W_gpu', W_gpu)
             else:
                 print('W_cpu.reshape(-1)[:20]', W_cpu.reshape(-1)[:20])
                 print('W_gpu.reshape(-1)[:20]', W_gpu.reshape(-1)[:20])
-            assert np.all(W_cpu == W_gpu)
+            assert np.all(np.abs(W_cpu - W_gpu) < 1e-4)
 
 
 @pytest.mark.parametrize(
     'shape',
     shapes)
 def test_random_normal(shape):
-    test_random_func('tf.random_normal', shape)
+    _test_random_func('tf.random_normal', shape)
 
 
 @pytest.mark.parametrize(
     'shape',
     shapes)
 def test_random_uniform(shape):
-    test_random_func('tf.random_uniform', shape)
+    _test_random_func('tf.random_uniform', shape)
 
 
 if __name__ == '__main__':
