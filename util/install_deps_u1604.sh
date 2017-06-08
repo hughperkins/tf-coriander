345ae534c2588e12560a1b41a78c28b00b703a67
Hugh Perkins <hughperkins@gmail.com>
2017-06-08 14:43:09 +0100

    install_deps_u1604.sh

--- util/install_deps_u1604.sh
+++ util/install_deps_u1604.sh
@@ -21,15 +21,35 @@ ${SUDO} apt-get update && ${SUDO} apt-get install -y --no-install-recommends \
     ca-certificates openjdk-8-jdk \
     ocl-icd-opencl-dev clinfo opencl-headers
 
+if [[ ! -d soft ]]; then {
+    mkdir soft
+} fi
 pushd soft
-wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-tar -xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz && \
-mv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04 llvm-4.0
-wget https://github.com/bazelbuild/bazel/releases/download/0.4.5/bazel_0.4.5-linux-x86_64.deb
-${SUDO} dpkg -i bazel_0.4.5-linux-x86_64.deb
-popd soft
 
-python3 -m virtualenv -p python3 env3
+if [[ ! -d llvm-4.0 ]]; then {
+    wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -O clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+    if [[ -d clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04 ]]; then {
+        rm -R clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04
+    } fi
+    tar -xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+    mv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04 llvm-4.0
+} fi
+
+set +e
+bazelversion=none
+bazelversion=$(bazel --batch version | grep 'Build label')
+set -e
+
+if [[ ${bazelversion} != 'Build label: 0.4.5' ]]; then {
+    wget https://github.com/bazelbuild/bazel/releases/download/0.4.5/bazel_0.4.5-linux-x86_64.deb -O bazel_0.4.5-linux-x86_64.deb
+    ${SUDO} dpkg -i bazel_0.4.5-linux-x86_64.deb
+} fi
+
+popd
+
+if [[ ! -d env3 ]]; then {
+    python3 -m virtualenv -p python3 env3
+} fi
 
 . env3/bin/activate
 pip install -r util/requirements.txt
