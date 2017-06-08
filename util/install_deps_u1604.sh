505b721f7ade227517fa5e55fab9f6a8413f848f
Hugh Perkins <hughperkins@gmail.com>
2017-06-08 14:15:31 +0100

    docker-friendly sudo in install_deps_u1604.sh

--- util/install_deps_u1604.sh
+++ util/install_deps_u1604.sh
@@ -3,7 +3,16 @@
 set -e
 set -x
 
-sudo apt-get update && apt-get install -y --no-install-recommends \
+SUDO=sudo
+if [[ $(cat /proc/1/sched | head -n 1 | grep bash) ]]; then {
+    # running in docker
+    SUDO=
+} fi
+if [[ $(uname) == Darwin ]]; then {
+    SUDO=
+} fi
+
+${SUDO} apt-get update && ${SUDO} apt-get install -y --no-install-recommends \
     cmake cmake-curses-gui git gcc g++ build-essential \
     libc6-dev zlib1g-dev libtinfo-dev \
     curl wget xz-utils unzip zip rsync \
@@ -17,7 +26,7 @@ wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.
 tar -xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz && \
 mv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04 llvm-4.0
 wget https://github.com/bazelbuild/bazel/releases/download/0.4.5/bazel_0.4.5-linux-x86_64.deb
-sudo dpkg -i bazel_0.4.5-linux-x86_64.deb
+${SUDO} dpkg -i bazel_0.4.5-linux-x86_64.deb
 popd soft
 
 python3 -m virtualenv -p python3 env3
